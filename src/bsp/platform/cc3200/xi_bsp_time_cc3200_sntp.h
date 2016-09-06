b031c103222fd56a16ab916c258615c5a5d7be1f
atigyi <tigyi.arpad@gmail.com>
2016-09-06 11:47:23 +0200

    [ cc3200 ] [ date/time ] providing accurate date/time on cc3200. New BSP TIME API function introduced: xi_bsp_time_init(). For POSIX this should do nothing. In MCU environments this init function may start a timer interrupt to extrapolate time originating from and SNTP request. Or interpolate the time between two SNTP requests. This solution is blocking e.g. the xi_bsp_time_init does not return unti the UDP SNTP request is not finished. No errorhandling done for SNTP request, the application may continue with uninitialized clock.

---
+++ src/bsp/platform/cc3200/xi_bsp_time_cc3200_sntp.h
@@ -0,0 +1,30 @@
+#ifndef __XI_BSP_TIME_CC3200_SNTP_H__
+#define __XI_BSP_TIME_CC3200_SNTP_H__
+
+#include <stdint.h>
+
+/******************************************************************************
+ *                                                                            *
+ *  Definitions                                                               *
+ *                                                                            *
+ ******************************************************************************/
+
+typedef int32_t   posix_time_t;
+typedef uint32_t  ntp_time_t;
+
+
+/******************************************************************************
+ *                                                                            *
+ * Externs                                                                    *
+ *                                                                            *
+ ******************************************************************************/
+
+extern ntp_time_t   sntp_time_ntp   (void); // Seconds since 1900/01/01 00:00:00
+extern posix_time_t sntp_time_posix (void); // Seconds since 1970/01/01 00:00:00
+
+void sntp_task (void  *pvParameters);
+
+static uint32_t    start_time_ntp  = 0;
+static uint32_t    uptime          = 0;
+
+#endif /* __XI_BSP_TIME_CC3200_SNTP_H__ */
