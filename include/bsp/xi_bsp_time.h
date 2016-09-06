162ceebb2753dc176125486891401aa95cb2f3bb
atigyi <tigyi.arpad@gmail.com>
2016-09-06 13:15:23 +0200

    [ doc ] [ bsp time ] doxygen description for xi_bsp_time_init function

--- include/bsp/xi_bsp_time.h
+++ include/bsp/xi_bsp_time.h
@@ -22,6 +22,14 @@
 extern "C" {
 #endif
 
+/**
+ * @function
+ * @brief Initialization of BSP TIME module.
+ *
+ * This function should contain initialization code to fire up time functionality on a
+ * certain platform. For POSIX systems usually this remains an empty function. For
+ * microcontrollers NTP and time-step code may reside here.
+ */
 void xi_bsp_time_init();
 
 /**
