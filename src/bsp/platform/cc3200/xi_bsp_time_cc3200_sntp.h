0ba1ca5befa9d0d2430836437c85d33ac411d4d5
atigyi <tigyi.arpad@gmail.com>
2016-09-07 11:55:22 +0200

    [ cc3200 ] [ review ] printf removal, adding doxygen doc to function headers in .h file

--- src/bsp/platform/cc3200/xi_bsp_time_cc3200_sntp.h
+++ src/bsp/platform/cc3200/xi_bsp_time_cc3200_sntp.h
@@ -19,9 +19,26 @@ typedef uint32_t  ntp_time_t;
  *                                                                            *
  ******************************************************************************/
 
+/**
+ * @function
+ * @brief Returns seconds since 1900/01/01 00:00:00
+ */
 extern ntp_time_t   sntp_time_ntp   (void); // Seconds since 1900/01/01 00:00:00
+
+/**
+ * @function
+ * @brief Returns seconds since 1970/01/01 00:00:00
+ */
 extern posix_time_t sntp_time_posix (void); // Seconds since 1970/01/01 00:00:00
 
+/**
+ * @function
+ * @brief Acquires date/time from NTP service.
+ *
+ * @param pvParameters ?
+ *
+ * Cycles through multiple NTP servers until date/time is acquired.
+ */
 void sntp_task (void  *pvParameters);
 
 static uint32_t    start_time_ntp  = 0;
