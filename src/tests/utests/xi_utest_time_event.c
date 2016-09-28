7daa7eb992ebece7174f400c2a1e1da0b1322174
olgierdh <olgierd.humenczuk@logmein.com>
2016-09-28 12:15:45 +0200

    [feat] added comments to the time event implementation
    	* fixed formatting

--- src/tests/utests/xi_utest_time_event.c
+++ src/tests/utests/xi_utest_time_event.c
@@ -13,47 +13,33 @@
 #define TEST_TIME_EVENT_TEST_SIZE 64
 void test_time_event()
 {
-    xi_vector_t* vector = xi_vector_create();
+}
 
-    xi_time_event_handle_t time_event_handles[TEST_TIME_EVENT_TEST_SIZE] = {
-        xi_make_empty_time_event_handle()};
-    xi_time_event_t time_events[TEST_TIME_EVENT_TEST_SIZE] = {xi_make_empty_time_event()};
+static xi_state_t fill_vector_with_heap_elements_linear_order(
+    xi_vector_t* vector,
+    xi_time_event_t ( *time_events )[TEST_TIME_EVENT_TEST_SIZE],
+    xi_time_event_handle_t ( *time_event_handles )[TEST_TIME_EVENT_TEST_SIZE] )
+{
+    xi_state_t state = XI_STATE_OK;
 
     int i = 0;
     for ( ; i < TEST_TIME_EVENT_TEST_SIZE; ++i )
     {
-        time_events[i].time_of_execution = ( xi_time_t )rand() % 1000;
+        time_events[0][i].time_of_execution = i;
 
         xi_state_t ret_state =
-            xi_time_event_add( vector, &time_events[i], &time_event_handles[i] );
+            xi_time_event_add( vector, &time_events[0][i], &time_event_handles[0][i] );
+
+        XI_CHECK_STATE( ret_state );
 
         tt_assert( ret_state == XI_STATE_OK );
-        tt_assert( time_event_handles[i].position != NULL );
+        tt_assert( time_event_handles[0][i].position != NULL );
+        tt_assert( *( time_event_handles[0][i].position ) == i );
     }
 
-    /* counter for tracking the number of elements */
-    int no_elements = 0;
-    /* for tracking the order of the heap */
-    int last_element_value = 0;
-
-    /* at this point we should have a heap constructed, we can test if taking elements
-     * from the top we will receive them in a sorted order */
-    do
-    {
-        xi_time_event_t* time_event = xi_time_event_get_top( vector );
-        tt_assert( time_event->time_of_execution >= last_element_value );
-        last_element_value = time_event->time_of_execution;
-        ++no_elements;
-        /* we have to relelase the memory */
-    } while ( vector->elem_no != 0 );
-
-    /* and we can check if all of them has been received */
-    tt_assert( no_elements == TEST_TIME_EVENT_TEST_SIZE );
-
-    xi_vector_destroy( vector );
-
-    tt_int_op( xi_is_whole_memory_deallocated(), >, 0 );
-end:;
+end:
+err_handling:
+    return state;
 }
 
 #endif
@@ -78,9 +64,86 @@ end:;
 
 } )
 
-XI_TT_TESTCASE( utest__xi_time_event_execute_handle_in__add_100_random_time_events,
-                { test_time_event(); } )
+XI_TT_TESTCASE(
+    utest__xi_time_event_execute_handle_in__add_TEST_TIME_EVENT_TEST_SIZE_random_time_events,
+    {
+        xi_vector_t* vector = xi_vector_create();
 
+        xi_time_event_handle_t time_event_handles[TEST_TIME_EVENT_TEST_SIZE] = {
+            xi_make_empty_time_event_handle()};
+        xi_time_event_t time_events[TEST_TIME_EVENT_TEST_SIZE] = {
+            xi_make_empty_time_event()};
+
+        int i = 0;
+        for ( ; i < TEST_TIME_EVENT_TEST_SIZE; ++i )
+        {
+            time_events[i].time_of_execution = ( xi_time_t )rand() % 1000;
+
+            xi_state_t ret_state =
+                xi_time_event_add( vector, &time_events[i], &time_event_handles[i] );
+
+            tt_assert( ret_state == XI_STATE_OK );
+            tt_assert( time_event_handles[i].position != NULL );
+        }
+
+        /* counter for tracking the number of elements */
+        int no_elements = 0;
+        /* for tracking the order of the heap */
+        int last_element_value = 0;
+
+        /* at this point we should have a heap constructed, we can test if taking elements
+         * from the top we will receive them in a sorted order */
+        do
+        {
+            xi_time_event_t* time_event = xi_time_event_get_top( vector );
+            tt_assert( time_event->time_of_execution >= last_element_value );
+            last_element_value = time_event->time_of_execution;
+            ++no_elements;
+            /* we have to relelase the memory */
+        } while ( vector->elem_no != 0 );
+
+        /* and we can check if all of them has been received */
+        tt_assert( no_elements == TEST_TIME_EVENT_TEST_SIZE );
+
+        xi_vector_destroy( vector );
+
+        tt_int_op( xi_is_whole_memory_deallocated(), >, 0 );
+    end:;
+    } )
+
+
+XI_TT_TESTCASE(
+    utest__xi_time_event_restart_first_element__event_key_and_position_changed, {
+        xi_vector_t* vector = xi_vector_create();
+
+        xi_time_event_handle_t time_event_handles[TEST_TIME_EVENT_TEST_SIZE] = {
+            xi_make_empty_time_event_handle()};
+        xi_time_event_t time_events[TEST_TIME_EVENT_TEST_SIZE] = {
+            xi_make_empty_time_event()};
+
+        xi_state_t ret_state = fill_vector_with_heap_elements_linear_order(
+            vector, &time_events, &time_event_handles );
+
+        tt_assert( XI_STATE_OK == ret_state );
+
+        const xi_time_t new_test_time = TEST_TIME_EVENT_TEST_SIZE + 12;
+
+        ret_state =
+            xi_time_event_restart( vector, &time_event_handles[0], new_test_time );
+
+        tt_assert( XI_STATE_OK == ret_state );
+
+        xi_time_event_t* time_event =
+            ( xi_time_event_t* )vector->array[TEST_TIME_EVENT_TEST_SIZE - 1]
+                .selector_t.ptr_value;
+
+        tt_assert( time_event->time_of_execution == new_test_time );
+        tt_assert( time_event->position == *time_event_handles[0].position );
+
+        xi_vector_destroy( vector );
+        tt_int_op( xi_is_whole_memory_deallocated(), >, 0 );
+    end:;
+    } )
 
 XI_TT_TESTGROUP_END
 
