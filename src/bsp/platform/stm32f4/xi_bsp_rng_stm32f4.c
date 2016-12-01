c825030a958a98229e4a774e5697004461762fba
olgierdh <olgierd.humenczuk@logmein.com>
2016-12-01 12:05:53 +0100

    [fix] CyaSSL -> WolfSSL

--- src/bsp/platform/stm32f4/xi_bsp_rng_stm32f4.c
+++ src/bsp/platform/stm32f4/xi_bsp_rng_stm32f4.c
@@ -25,8 +25,8 @@ void xi_bsp_rng_shutdown()
 
 #elif defined( XI_TLS_LIB_WOLFSSL ) /* WOLFSSL version of RNG implementation */
 
-#include <cyassl/ctaocrypt/random.h>
-#include <cyassl/ctaocrypt/memory.h>
+#include <wolfssl/wolfcrypt/random.h>
+#include <wolfssl/wolfcrypt/memory.h>
 #include <xi_allocator.h>
 
 static WC_RNG wolfcrypt_rng;
@@ -36,7 +36,7 @@ void xi_bsp_rng_init()
     /* check if already initialized */
     if ( NULL == wolfcrypt_rng.drbg )
     {
-        CyaSSL_SetAllocators( xi_alloc_ptr, xi_free_ptr, xi_realloc_ptr );
+        wolfSSL_SetAllocators( xi_alloc_ptr, xi_free_ptr, xi_realloc_ptr );
 
         wc_InitRng( &wolfcrypt_rng );
     }
