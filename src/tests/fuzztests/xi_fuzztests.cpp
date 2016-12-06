65c8c34bdf88424844d0eebeffd7e8ab0f4c5982
olgierdh <olgierd.humenczuk@logmein.com>
2016-12-06 18:09:58 +0100

    [feat] removed logging from the fuzz test

--- src/tests/fuzztests/xi_fuzztests.cpp
+++ src/tests/fuzztests/xi_fuzztests.cpp
@@ -11,8 +11,6 @@ const static size_t XI_BUFFER_SIZE = 32;
 
 extern "C" int LLVMFuzzerTestOneInput( const uint8_t* data, size_t size )
 {
-    xi_debug_format( "entering size %d", size );
-
     xi_mqtt_parser_t parser;
     xi_state_t local_state = XI_STATE_OK;
     xi_mqtt_message_t* msg = NULL;
@@ -22,13 +20,9 @@ extern "C" int LLVMFuzzerTestOneInput( const uint8_t* data, size_t size )
     XI_ALLOC_AT( xi_mqtt_message_t, msg, local_state );
     xi_mqtt_parser_init( &parser );
 
-    xi_debug_logger( "======================================================" );
-
     while ( size_eaten != size )
     {
         const size_t copied_buffer_size = XI_MIN( XI_BUFFER_SIZE, ( size - size_eaten ) ); 
-
-        xi_debug_format( "copied_buffer_size: %d", copied_buffer_size );
 
         xi_data_desc_t* data_desc =
             xi_make_desc_from_buffer_copy( data + size_eaten, copied_buffer_size );
@@ -41,12 +35,7 @@ extern "C" int LLVMFuzzerTestOneInput( const uint8_t* data, size_t size )
             XI_ALLOC_AT( xi_mqtt_message_t, msg, local_state );
         }
 
-        xi_debug_format( "data_desc->length: %d, data_desc->curr_pos: %d, copied_buffer_size: %d", data_desc->length,
-                         data_desc->curr_pos, copied_buffer_size );
-        
         size_eaten += copied_buffer_size - ( data_desc->length - data_desc->curr_pos );
-        
-        xi_debug_format( "size_eaten: %d, state: %d", size_eaten, local_state );
             
         xi_free_desc( &data_desc );
     }
