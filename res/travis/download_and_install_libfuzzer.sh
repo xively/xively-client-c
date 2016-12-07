2cef2f6f3acce043bdf5a9e1dde837355a190bf8
olgierdh <olgierd.humenczuk@logmein.com>
2016-12-07 10:54:36 +0100

    [tidy] cleaning build scripts

---
+++ res/travis/download_and_install_libfuzzer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+LIBFUZZER_DOWNLOAD_DIR=$1
+LIBFUZZER_INSTALL_DIR=$2
+
+mkdir -p $LIBFUZZER_DOWNLOAD_DIR
+mkdir -p $LIBFUZZER_INSTALL_DIR
+
+git clone https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer $LIBFUZZER_DOWNLOAD_DIR
+
+cd $LIBFUZZER_DOWNLOAD_DIR
+#clang++ -c -g -v -O2 -lstdc++ -std=c++11 *.cpp -IFuzzer
+#ar ruv libFuzzer.a Fuzzer*.o
+./build.sh
+
+cp libFuzzer.a $LIBFUZZER_INSTALL_DIR
+
+
