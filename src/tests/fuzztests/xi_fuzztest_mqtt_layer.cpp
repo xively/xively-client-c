6af12037c046ed741c5e9cd33c64877351c01fda
olgierdh <olgierd.humenczuk@logmein.com>
2016-12-13 16:17:22 +0100

    [doc] added comments about the llvm fuzz test main function signature

--- src/tests/fuzztests/xi_fuzztest_mqtt_layer.cpp
+++ src/tests/fuzztests/xi_fuzztest_mqtt_layer.cpp
@@ -3,9 +3,9 @@
 #include <stdint.h>
 #include <string.h>
 
+/* This is the fuzzer signature, and we cannot change it */
 extern "C" int LLVMFuzzerTestOneInput( const uint8_t* data, size_t size )
 {
     /* not implemented */
     return 0;
 }
-
--- src/tests/fuzztests/xi_fuzztest_mqtt_parser.cpp
+++ src/tests/fuzztests/xi_fuzztest_mqtt_parser.cpp
@@ -9,6 +9,7 @@
 
 const static size_t XI_BUFFER_SIZE = 32;
 
+/* This is the fuzzer signature, and we cannot change it */
 extern "C" int LLVMFuzzerTestOneInput( const uint8_t* data, size_t size )
 {
     xi_mqtt_parser_t parser;
@@ -22,21 +23,21 @@ extern "C" int LLVMFuzzerTestOneInput( const uint8_t* data, size_t size )
 
     while ( size_eaten != size )
     {
-        const size_t copied_buffer_size = XI_MIN( XI_BUFFER_SIZE, ( size - size_eaten ) ); 
+        const size_t copied_buffer_size = XI_MIN( XI_BUFFER_SIZE, ( size - size_eaten ) );
 
         xi_data_desc_t* data_desc =
             xi_make_desc_from_buffer_copy( data + size_eaten, copied_buffer_size );
 
         local_state = xi_mqtt_parser_execute( &parser, msg, data_desc );
 
-        if( local_state == XI_STATE_OK || local_state == XI_MQTT_PARSER_ERROR )
+        if ( local_state == XI_STATE_OK || local_state == XI_MQTT_PARSER_ERROR )
         {
             xi_mqtt_message_free( &msg );
             XI_ALLOC_AT( xi_mqtt_message_t, msg, local_state );
         }
 
         size_eaten += copied_buffer_size - ( data_desc->length - data_desc->curr_pos );
-            
+
         xi_free_desc( &data_desc );
     }
 
@@ -45,4 +46,3 @@ extern "C" int LLVMFuzzerTestOneInput( const uint8_t* data, size_t size )
 err_handling:
     return 0;
 }
-
