7c3929df43b6e697423c62f783a9c41e7bfb1d5e
atigyi <tigyi.arpad@gmail.com>
2017-01-11 11:52:29 +0100

    [ stm port ] directing Travis STM32 builds to use STM32Cube v1.14 instead of the previous v1.13

--- make/mt-os/mt-stm32f4.mk
+++ make/mt-os/mt-stm32f4.mk
@@ -6,21 +6,23 @@
 include make/mt-os/mt-os-common.mk
 include make/mt-utils/mt-get-gnu-arm-toolchain.mk
 
-XI_STM32_PATH_SDK = $(HOME)/Downloads/xively-client-artifactory/st/STM32Cube_FW_F4_V1.13.0
+XI_STM32_PATH_SDK = $(HOME)/Downloads/xively-client-artifactory/st/STM32Cube_FW_F4_V1.14.0
 
 
 #####################
 # LWIP configurations
 #####################
-XI_STM32_PATH_SDK_LWIP = $(XI_STM32_PATH_SDK)/Middlewares/Third_Party/LwIP
 
-XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK_LWIP)/src/include
-XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK_LWIP)/system
+XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Middlewares/Third_Party/LwIP/src/include
+XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Middlewares/Third_Party/LwIP/system
+
 XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Middlewares/Third_Party/FreeRTOS/Source/include
 XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F
+
 XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Drivers/STM32F4xx_HAL_Driver/Inc
 XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Drivers/CMSIS/Device/ST/STM32F4xx/Include
 XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Drivers/CMSIS/Include
+
 XI_COMPILER_FLAGS += -I$(LIBXIVELY)/examples/stm32/xively_demo_mt/Inc
 
 ####################
