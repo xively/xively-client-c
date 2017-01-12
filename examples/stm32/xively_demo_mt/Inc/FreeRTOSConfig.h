8d353fc6ecfe2e9b72eaaf2407e54e1384a8660b
Arpad Tigyi <atigyi@logmein.com>
2017-01-12 16:00:21 +0100

    Feature/stm32 bsp accurate time through SNTP servers (#83)
    [DESCRIPTION]
    This change includes an SNTP UDP request towards SNTP server to get accurate time. In case the server replies the STM32 board will have accurate time. In case of no accurate time available the loading of GlobalSign Root CA cert will fail. Before this change one had to provide a static timestamp to make the library work. Now if SNTP server is available it is automatic.
    
    [REVIEWERS]
    
    [JIRA]
    https://jira.3amlabs.net/browse/XCL-2601
    
    [QA]
    Tested the example application on stm32f41g evalutation board.
    
    [RELEASE NOTES]
    Providing accurate time for STM32 boards at board startup. Done with BSP TIME for STM32. Using SNTP servers.
    
    * [ stm port ] making STM3241G-eval board connect to Xively with fine-tuning memory settings
    
    * [ stm port ] minor correction in BSP TLS wolfssl
    
    * [ stm port ] BSP MEM STM32 implementation, using FreeRTOS's memory handler functions: pvPortMalloc, vPortFree. Note: realloc is not implemented.
    
    * [ stm port ] updating mt-stm32 config file with fair enough wolfSSL flags
    
    * [ stm port ] cutting wolfSSL flags in mt-stm32f4.mk seemingly to minimal set == complexity reduction of this file
    
    * [ stm port ] removing own copy of LwIP_HTTP_Server_Socket_RTOS example's untouched files. Instead links to STM32Cube's files are added. This will provide more cross-sdk compatibility
    
    * [ stm port ] atigyi project paths set in into eclipse demo project
    
    * [ stm port ] a FreeRTOSConfig memory settings which work for STM3241G-EVAL board
    
    * [ stm port ] udating static XTIME function
    
    * [ stm port ] removing SDK specific example header files from own repo, they are linked from the SDK itself instead to be one step more robust on SDK change
    
    * [ stm port ] setting a custom MAC address to avoid conflicting IP got from DHCP server on same LAN for multiple boards
    
    * [ stm port ] initial SNTP solution (CC3200) using FreeRTOS tasks
    
    * [ stm port ] stabilizing STM3241G-EVAL application execution with raising XC_TASK_STACK value
    
    * [ stm port ] STNP + uptime implementation
    
    * [ stm port ] STM32 Travis build fix: fixing the path to STM32Cube
    
    * [ stm port ] directing Travis STM32 builds to use STM32Cube v1.14 instead of the previous v1.13
    
    * [ stm port ] using the same SNTP implementation files as TI CC3200 SNTP solution uses, just ported to STM32
    
    * [ stm port ] review change

--- examples/stm32/xively_demo_mt/Inc/FreeRTOSConfig.h
+++ examples/stm32/xively_demo_mt/Inc/FreeRTOSConfig.h
@@ -92,11 +92,11 @@ extern uint32_t SystemCoreClock;
 #define configCPU_CLOCK_HZ ( SystemCoreClock )
 #define configTICK_RATE_HZ ( ( TickType_t )1000 )
 #define configMAX_PRIORITIES ( 7 )
-#define configMINIMAL_STACK_SIZE ( ( uint16_t )128 )
+#define configMINIMAL_STACK_SIZE ( ( uint16_t )256 )
 #if defined( __GNUC__ )
-#define configTOTAL_HEAP_SIZE ( ( size_t )( 32 * 1024 ) )
+#define configTOTAL_HEAP_SIZE ( ( size_t )( 55 * 1024 ) )
 #else
-#define configTOTAL_HEAP_SIZE ( ( size_t )( 32 * 1024 ) )
+#define configTOTAL_HEAP_SIZE ( ( size_t )( 55 * 1024 ) )
 #endif
 #define configMAX_TASK_NAME_LEN ( 16 )
 #define configUSE_TRACE_FACILITY 1
