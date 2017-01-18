a64cbaab05c4d7138f77353bab36809c91239504
Milan Toth <milgra@Milans-MacBook-Pro.local>
2017-01-18 16:44:15 +0100

    code formatting added, bsp proxy function added to a separate header file in a separate folder

---
+++ src/bsp/platform/stm32f4_nucleo_wifi/includes/xi_bsp_io_net_socket_proxy.h
@@ -0,0 +1,2 @@
+void xi_bsp_io_net_socket_data_received_proxy(uint8_t socket_id, uint8_t * data_ptr, uint32_t message_size, uint32_t chunk_size);
+void xi_bsp_io_net_socket_client_remote_server_closed_proxy(uint8_t * socket_closed_id);
--- src/bsp/platform/stm32f4_nucleo_wifi/xi_bsp_io_net_stm32f4_nucleo_wifi.c
+++ src/bsp/platform/stm32f4_nucleo_wifi/xi_bsp_io_net_stm32f4_nucleo_wifi.c
@@ -20,18 +20,19 @@ extern "C" {
 #define XI_BSP_IO_NET_BUFFER_SIZE 512
 
 typedef enum {
-  xi_wifi_state_connected = 0,
-  xi_wifi_state_disconnected,
-  xi_wifi_state_error,
+    xi_wifi_state_connected = 0,
+    xi_wifi_state_disconnected,
+    xi_wifi_state_error,
 } xi_bsp_stm_wifi_state_t;
 
-typedef struct _xi_bsp_stm_wifi_buffer_t {
-  uint8_t bytes[ XI_BSP_IO_NET_BUFFER_SIZE ];
-  uint32_t size;
+typedef struct _xi_bsp_stm_wifi_buffer_t
+{
+    uint8_t bytes[XI_BSP_IO_NET_BUFFER_SIZE];
+    uint32_t size;
 } xi_bsp_stm_wifi_buffer_t;
 
-xi_bsp_stm_wifi_state_t xi_wifi_state = xi_wifi_state_connected;
-xi_bsp_stm_wifi_buffer_t xi_wifi_buffer = { 0 };
+xi_bsp_stm_wifi_state_t xi_wifi_state   = xi_wifi_state_connected;
+xi_bsp_stm_wifi_buffer_t xi_wifi_buffer = {0};
 
 xi_bsp_io_net_state_t xi_bsp_io_net_create_socket( xi_bsp_socket_t* xi_socket )
 {
@@ -49,13 +50,20 @@ xi_bsp_io_net_connect( xi_bsp_socket_t* xi_socket, const char* host, uint16_t po
     ( void )host;
     ( void )port;
 
-    char *protocol = "s";//t -> tcp , s-> secure tcp, c-> secure tcp with certs
+    char* protocol = "s"; // t -> tcp , s-> secure tcp, c-> secure tcp with certs
 
     WiFi_Status_t status = WiFi_MODULE_SUCCESS;
-    status = wifi_socket_client_open((uint8_t *)"broker.dev.xively.io", port, (uint8_t *)protocol, (uint8_t *)xi_socket);
+    status = wifi_socket_client_open( ( uint8_t* )"broker.dev.xively.io", port,
+                                      ( uint8_t* )protocol, ( uint8_t* )xi_socket );
 
-    if ( status == WiFi_MODULE_SUCCESS ) return XI_BSP_IO_NET_STATE_OK;
-    else return XI_BSP_IO_NET_STATE_ERROR;
+    if ( status == WiFi_MODULE_SUCCESS )
+    {
+        return XI_BSP_IO_NET_STATE_OK;
+    }
+    else
+    {
+        return XI_BSP_IO_NET_STATE_ERROR;
+    }
 }
 
 xi_bsp_io_net_state_t xi_bsp_io_net_connection_check( xi_bsp_socket_t xi_socket,
@@ -66,8 +74,14 @@ xi_bsp_io_net_state_t xi_bsp_io_net_connection_check( xi_bsp_socket_t xi_socket,
     ( void )host;
     ( void )port;
 
-    if ( xi_wifi_state == xi_wifi_state_connected ) return XI_BSP_IO_NET_STATE_OK;
-    else return XI_BSP_IO_NET_STATE_ERROR;
+    if ( xi_wifi_state == xi_wifi_state_connected )
+    {
+        return XI_BSP_IO_NET_STATE_OK;
+    }
+    else
+    {
+        return XI_BSP_IO_NET_STATE_ERROR;
+    }
 }
 
 xi_bsp_io_net_state_t xi_bsp_io_net_write( xi_bsp_socket_t xi_socket,
@@ -80,10 +94,16 @@ xi_bsp_io_net_state_t xi_bsp_io_net_write( xi_bsp_socket_t xi_socket,
     *out_written_count = count;
 
     WiFi_Status_t status = WiFi_MODULE_SUCCESS;
-    status = wifi_socket_client_write(xi_socket, count, (char*) buf);
+    status               = wifi_socket_client_write( xi_socket, count, ( char* )buf );
 
-    if ( status == WiFi_MODULE_SUCCESS ) return XI_BSP_IO_NET_STATE_OK;
-    else return XI_BSP_IO_NET_STATE_ERROR;
+    if ( status == WiFi_MODULE_SUCCESS )
+    {
+        return XI_BSP_IO_NET_STATE_OK;
+    }
+    else
+    {
+        return XI_BSP_IO_NET_STATE_ERROR;
+    }
 }
 
 xi_bsp_io_net_state_t xi_bsp_io_net_read( xi_bsp_socket_t xi_socket,
@@ -99,21 +119,28 @@ xi_bsp_io_net_state_t xi_bsp_io_net_read( xi_bsp_socket_t xi_socket,
 
     if ( xi_wifi_state == xi_wifi_state_connected )
     {
-      if ( xi_wifi_buffer.size > 0 )
-      {
-          memcpy( buf , xi_wifi_buffer.bytes , xi_wifi_buffer.size );
-          *out_read_count = xi_wifi_buffer.size;
-          xi_wifi_buffer.size = 0;
-      }
-      else return XI_BSP_IO_NET_STATE_BUSY;
+        if ( xi_wifi_buffer.size == 0 )
+        {
+            return XI_BSP_IO_NET_STATE_BUSY;
+        }
+        else if ( xi_wifi_buffer.size > count )
+        {
+            return XI_BSP_IO_NET_STATE_ERROR;
+        }
+        else
+        {
+            memcpy( buf, xi_wifi_buffer.bytes, xi_wifi_buffer.size );
+            *out_read_count     = xi_wifi_buffer.size;
+            xi_wifi_buffer.size = 0;
+        }
     }
     else if ( xi_wifi_state == xi_wifi_state_error )
     {
-      return XI_BSP_IO_NET_STATE_ERROR;    
+        return XI_BSP_IO_NET_STATE_ERROR;
     }
     else if ( xi_wifi_state == xi_wifi_state_connected )
     {
-      return XI_BSP_IO_NET_STATE_CONNECTION_RESET;    
+        return XI_BSP_IO_NET_STATE_CONNECTION_RESET;
     }
 
     return XI_BSP_IO_NET_STATE_OK;
@@ -122,30 +149,38 @@ xi_bsp_io_net_state_t xi_bsp_io_net_read( xi_bsp_socket_t xi_socket,
 xi_bsp_io_net_state_t xi_bsp_io_net_close_socket( xi_bsp_socket_t* xi_socket )
 {
     WiFi_Status_t status = WiFi_MODULE_SUCCESS;
-    status = wifi_socket_client_close( *xi_socket );
+    status               = wifi_socket_client_close( *xi_socket );
 
-    if ( status == WiFi_MODULE_SUCCESS ) return XI_BSP_IO_NET_STATE_OK;
-    else return XI_BSP_IO_NET_STATE_ERROR;
+    if ( status == WiFi_MODULE_SUCCESS )
+    {
+        return XI_BSP_IO_NET_STATE_OK;
+    }
+    else
+    {
+        return XI_BSP_IO_NET_STATE_ERROR;
+    }
 }
 
 xi_bsp_io_net_state_t xi_bsp_io_net_select( xi_bsp_socket_events_t* socket_events_array,
                                             size_t socket_events_array_size,
                                             long timeout_sec )
 {
-    (void)timeout_sec;
+    ( void )timeout_sec;
 
     size_t socket_id = 0;
 
     if ( xi_wifi_state == xi_wifi_state_connected )
     {
-
         for ( socket_id = 0; socket_id < socket_events_array_size; ++socket_id )
         {
             xi_bsp_socket_events_t* socket_events = &socket_events_array[socket_id];
 
             if ( 1 == socket_events->in_socket_want_read )
             {
-                if ( xi_wifi_buffer.size > 0 ) socket_events->out_socket_can_read = 1;
+                if ( xi_wifi_buffer.size > 0 )
+                {
+                    socket_events->out_socket_can_read = 1;
+                }
             }
 
             if ( 1 == socket_events->in_socket_want_connect )
@@ -160,39 +195,41 @@ xi_bsp_io_net_state_t xi_bsp_io_net_select( xi_bsp_socket_events_t* socket_event
         }
 
         return XI_BSP_IO_NET_STATE_OK;
-
     }
     else
     {
-      return XI_BSP_IO_NET_STATE_ERROR;
+        return XI_BSP_IO_NET_STATE_ERROR;
     }
-  }
+}
 
-  void xi_bsp_io_net_socket_data_received_proxy(uint8_t socket_id, uint8_t * data_ptr, uint32_t message_size, uint32_t chunk_size)
-  {
-    (void) socket_id;
+void xi_bsp_io_net_socket_data_received_proxy( uint8_t socket_id,
+                                               uint8_t* data_ptr,
+                                               uint32_t message_size,
+                                               uint32_t chunk_size )
+{
+    ( void )socket_id;
     if ( xi_wifi_buffer.size == 0 )
     {
         if ( chunk_size > XI_BSP_IO_NET_BUFFER_SIZE )
         {
-          // payload is too big
-          xi_wifi_state = xi_wifi_state_error;
+            // payload is too big
+            xi_wifi_state = xi_wifi_state_error;
         }
         else
         {
-          memcpy( xi_wifi_buffer.bytes, data_ptr, message_size );
-          xi_wifi_buffer.size = chunk_size;
+            memcpy( xi_wifi_buffer.bytes, data_ptr, message_size );
+            xi_wifi_buffer.size = chunk_size;
         }
     }
-    else 
+    else
     {
         // previous data is not processed yet
         xi_wifi_state = xi_wifi_state_error;
     }
-  }
+}
 
-  void xi_bsp_io_net_socket_client_remote_server_closed_proxy(uint8_t * socket_closed_id)
-  {
-    (void) socket_closed_id;
+void xi_bsp_io_net_socket_client_remote_server_closed_proxy( uint8_t* socket_closed_id )
+{
+    ( void )socket_closed_id;
     xi_wifi_state = xi_wifi_state_disconnected;
-  }
+}
