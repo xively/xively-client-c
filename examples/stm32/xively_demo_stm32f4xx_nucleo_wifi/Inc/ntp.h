0cfa8a9f7d2a6ae76aa7cb8e0f77940d0ae78efe
Juan Carlos Jimenez <jcjc.dev@gmail.com>
2017-01-25 17:27:47 +0000

    Fix handling of the UDP socket used for SNTP, get SNTP running

--- examples/stm32/xively_demo_stm32f4xx_nucleo_wifi/Inc/ntp.h
+++ examples/stm32/xively_demo_stm32f4xx_nucleo_wifi/Inc/ntp.h
@@ -1,6 +1,9 @@
 #ifndef __NTP_H__
 #define __NTP_H__
 
+#define SNTP_MSG_SIZE 48
+#define MAX_AT_CMD_SIZE 128
+
 WiFi_Status_t sntp_start( uint8_t* sock_id );
 WiFi_Status_t sntp_connect( char* sntp_server,
                             uint32_t sntp_port,
