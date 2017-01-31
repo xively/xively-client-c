d89a1d7404929efd290facc260217d84ae5c0ae4
Juan Carlos Jimenez <jcjc.dev@gmail.com>
2017-01-31 13:53:13 +0000

    Implement memory allocator for sntp_response_t

--- examples/stm32/xively_demo_stm32f4xx_nucleo_wifi/Inc/ntp.h
+++ examples/stm32/xively_demo_stm32f4xx_nucleo_wifi/Inc/ntp.h
@@ -10,19 +10,19 @@
 #define SNTP_DISCONNECTION_MSG "\x20\x02\x00\x03"
 
 typedef enum {
-    SNTP_INTERNAL_ERROR     = -6,
-    SNTP_NOT_AVAILABLE      = -5,
-    SNTP_PARSER_ERROR       = -4,
-    SNTP_REQUEST_FAILURE    = -3,
-    SNTP_SOCKET_ERROR       = -2,
-    SNTP_TIMEOUT            = -1,
-    SNTP_SUCCESS            = 0,
+    SNTP_INTERNAL_ERROR  = -6,
+    SNTP_NOT_AVAILABLE   = -5,
+    SNTP_PARSER_ERROR    = -4,
+    SNTP_REQUEST_FAILURE = -3,
+    SNTP_SOCKET_ERROR    = -2,
+    SNTP_TIMEOUT         = -1,
+    SNTP_SUCCESS         = 0,
 } sntp_status_t;
 
 typedef struct {
     uint8_t socket_id;
     char* response;
-} sntp_response;
+} sntp_response_t;
 
 sntp_status_t sntp_get_datetime( uint8_t* socket_id, int32_t* epoch_time );
 void sntp_socket_data_callback( uint8_t socket_id, uint8_t* data_ptr,
