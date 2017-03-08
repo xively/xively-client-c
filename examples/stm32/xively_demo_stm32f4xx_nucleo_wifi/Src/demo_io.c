c432e8f8d6cacd7a79a2c7b630e85eee48b5cf0c
Juan Carlos Jimenez <jcjc.dev@gmail.com>
2017-03-08 16:30:00 +0000

    Improve uart logging in demo_io.c, add missing messages

--- examples/stm32/xively_demo_stm32f4xx_nucleo_wifi/Src/demo_io.c
+++ examples/stm32/xively_demo_stm32f4xx_nucleo_wifi/Src/demo_io.c
@@ -400,6 +400,7 @@ int8_t io_float_to_string( float input, char* buf , int32_t buf_size )
     retv = snprintf( buf, buf_size, "%ld.%02ld", input_integer, input_fractional );
     if ( retv >= buf_size )
     {
+        printf( "\r\n\t[ERROR] trying to build string from float value" );
         return -1;
     }
     return 0;
@@ -428,6 +429,7 @@ int8_t io_axes_to_json( SensorAxes_t axes, char* buf , int32_t buf_size )
                      axes.AXIS_X, axes.AXIS_Y, axes.AXIS_Z );
     if ( retv >= buf_size )
     {
+        printf( "\r\n\t[ERROR] trying to create JSON string from sensor input" );
         return -1;
     }
     return 0;
