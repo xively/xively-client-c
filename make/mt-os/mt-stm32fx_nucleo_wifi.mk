5dde0c95ebc56797161b56a274e987d7c048ad02
Juan Carlos Jimenez <jcjc.dev@gmail.com>
2017-04-07 16:14:16 +0100

    Document Arpi`s stm32_spwf_wifi.h include workaround in the BSP

--- make/mt-os/mt-stm32fx_nucleo_wifi.mk
+++ make/mt-os/mt-stm32fx_nucleo_wifi.mk
@@ -8,6 +8,8 @@ include make/mt-utils/mt-get-gnu-arm-toolchain.mk
 
 XI_STM32_PATH_SDK = $(HOME)/Downloads/xively-client-artifactory/st/STM32CubeExpansion_WIFI1_V3.0.1
 
+# This BSP folder MUST be included before the SDK's X-NUCLEO-IDW0xx1/.
+# More info available in src/bsp/platform/stm32fx_nucleo_wifi/stm32_spwf_wifi.h
 XI_COMPILER_FLAGS += -I$(XI_BSP_DIR)/platform/$(XI_BSP_PLATFORM)
 XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Middlewares/ST/STM32_SPWF0xSy/Utils
 XI_COMPILER_FLAGS += -I$(XI_STM32_PATH_SDK)/Middlewares/ST/STM32_SPWF0xSy/Inc
