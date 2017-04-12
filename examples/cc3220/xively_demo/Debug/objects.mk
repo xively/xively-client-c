0ae940b810ad4e24700ff30a201945aabe7a1b39
David Della Bitta <ddellabitta@logmein.com>
2017-04-12 18:24:25 -0400

    CC3220 example project in CCS

---
+++ examples/cc3220/xively_demo/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/ti/simplelink_cc3220_sdk_1_01_02_00/source/ti/drivers/net/wifi/ccs/tirtos/simplelink.a" -l"E:/dev/xively/xively-client-c/bin/cc3220/libxively.a" -l"C:/ti/simplelink_cc3220_sdk_1_01_02_00/source/ti/devices/cc32xx/driverlib/ccs/Release/driverlib.a" -llibc.a
+
---
+++ examples/cc3220/xively_demo/Debug/simplelinkWifi/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../simplelinkWifi/provisioning_task.c \
+../simplelinkWifi/simplelink_callbacks.c 
+
+C_DEPS += \
+./simplelinkWifi/provisioning_task.d \
+./simplelinkWifi/simplelink_callbacks.d 
+
+OBJS += \
+./simplelinkWifi/provisioning_task.obj \
+./simplelinkWifi/simplelink_callbacks.obj 
+
+OBJS__QUOTED += \
+"simplelinkWifi\provisioning_task.obj" \
+"simplelinkWifi\simplelink_callbacks.obj" 
+
+C_DEPS__QUOTED += \
+"simplelinkWifi\provisioning_task.d" \
+"simplelinkWifi\simplelink_callbacks.d" 
+
+C_SRCS__QUOTED += \
+"../simplelinkWifi/provisioning_task.c" \
+"../simplelinkWifi/simplelink_callbacks.c" 
+
+
