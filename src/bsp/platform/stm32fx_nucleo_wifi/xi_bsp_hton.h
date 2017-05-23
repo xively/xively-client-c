d545fcd30a5d645ffcaee85a31106975d5780427
atigyi <tigyi.arpad@gmail.com>
2017-05-23 11:48:57 +0200

    [ SFT ] adding cn-cbor support for STM32 wifi

---
+++ src/bsp/platform/stm32fx_nucleo_wifi/xi_bsp_hton.h
@@ -0,0 +1,59 @@
+#ifndef __XI_BSP_HTON_H__
+#define __XI_BSP_HTON_H__
+
+#include <stdint.h>
+#include <stddef.h> /* for size_t */
+typedef long int ssize_t;
+
+/**
+ * Convert an uint16_t from host- to network byte order.
+ *
+ * @param n uint16_t in host byte order
+ * @return n in network byte order
+ */
+uint16_t
+htons(uint16_t n)
+{
+  return ((n & 0xff) << 8) | ((n & 0xff00) >> 8);
+}
+
+/**
+ * Convert an uint16_t from network- to host byte order.
+ *
+ * @param n uint16_t in network byte order
+ * @return n in host byte order
+ */
+uint16_t
+ntohs(uint16_t n)
+{
+  return htons(n);
+}
+
+/**
+ * Convert an uint32_t from host- to network byte order.
+ *
+ * @param n uint32_t in host byte order
+ * @return n in network byte order
+ */
+uint32_t
+htonl(uint32_t n)
+{
+  return ((n & 0xff) << 24) |
+    ((n & 0xff00) << 8) |
+    ((n & 0xff0000UL) >> 8) |
+    ((n & 0xff000000UL) >> 24);
+}
+
+/**
+ * Convert an uint32_t from network- to host byte order.
+ *
+ * @param n uint32_t in network byte order
+ * @return n in host byte order
+ */
+uint32_t
+ntohl(uint32_t n)
+{
+  return htonl(n);
+}
+
+#endif /* __XI_BSP_HTON_H__ */
