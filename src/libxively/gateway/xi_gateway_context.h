b4f82cc9ad5518de7fd18fc5a3a4b7f9ec991c0d
atigyi <tigyi.arpad@gmail.com>
2018-01-10 12:51:05 +0100

    [ GW ] gateway context generator and destructor functions filled, executed by the - for now - sole itest, no leak

--- src/libxively/gateway/xi_gateway_context.h
+++ src/libxively/gateway/xi_gateway_context.h
@@ -9,16 +9,21 @@
 
 #include <xi_layer_type.h>
 #include <xi_layer_chain.h>
+#include <xi_types.h>
 
 typedef struct xi_gateway_context_s
 {
     xi_layer_chain_t layer_chain;
-
+    xi_context_data_t context_data;
 } xi_gateway_context_t;
 
 xi_state_t xi_create_gateway_context_with_custom_layers( xi_gateway_context_t** context,
                                                          xi_layer_type_t layer_config[],
                                                          xi_layer_type_id_t layer_chain[],
+                                                         size_t layer_chain_size );
+
+xi_state_t xi_delete_gateway_context_with_custom_layers( xi_gateway_context_t** context,
+                                                         xi_layer_type_t layer_config[],
                                                          size_t layer_chain_size );
 
 
