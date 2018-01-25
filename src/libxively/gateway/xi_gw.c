7660dc68d109644d22ff3c37f0f9e132fa0a9877
atigyi <tigyi.arpad@gmail.com>
2018-01-25 15:40:51 +0100

    [ GW ] bugfix: object was deallocated after putting it into the container. Which lead to the usage of a deallocated memory block which was hard to identify. But now the fix is there. Also ed_disconnect deletes the edge device’s context.

--- src/libxively/gateway/xi_gw.c
+++ src/libxively/gateway/xi_gw.c
@@ -13,17 +13,17 @@
 #include <xi_gateway_context.h>
 
 #include <xi_vector.h>
+#include <xi_helpers.h>
 
 typedef struct xi_ed_id_ed_context_pair_s
 {
-    const char* edge_device_id;
-    xi_context_handle_t edge_device_context;
+    char* edge_device_id;
+    xi_context_t* edge_device_context;
 } xi_ed_id_ed_context_pair_t;
 
 static int8_t _xi_ed_find_context_2_id( const union xi_vector_selector_u* e0,
                                         const union xi_vector_selector_u* e1 )
 {
-    printf( "-- %s ---, e0: %p, e1: %p\n", __FUNCTION__, e0, e1 );
     if ( NULL == e0 || NULL == e1 )
     {
         return 1;
@@ -46,10 +46,11 @@ xi_state_t xi_connect_ed( xi_context_handle_t xih, const char* edge_device_id )
 {
     ( void )xih;
 
-    xi_state_t state = XI_STATE_OK;
+    xi_state_t state                          = XI_STATE_OK;
+    xi_ed_id_ed_context_pair_t* ed_id_context = NULL;
 
-    xi_ed_id_ed_context_pair_t search_key = {.edge_device_id = edge_device_id,
-                                             XI_INVALID_CONTEXT_HANDLE};
+    xi_ed_id_ed_context_pair_t search_key = {.edge_device_id = ( char* )edge_device_id,
+                                             .edge_device_context = NULL};
 
     const xi_vector_index_type_t found_index = xi_vector_find(
         xi_globals.context_handles_vector_edge_devices,
@@ -58,17 +59,28 @@ xi_state_t xi_connect_ed( xi_context_handle_t xih, const char* edge_device_id )
     if ( -1 == found_index )
     {
         /* context not found to this application device id, create a new context */
-        xi_context_t* context = NULL;
 
-        XI_CHECK_STATE( state = xi_create_context_with_custom_layers_and_evtd(
-                            &context, xi_gw_layer_chain, XI_LAYER_CHAIN_GW,
-                            XI_LAYER_CHAIN_SCHEME_LENGTH( XI_LAYER_CHAIN_GW ), NULL,
-                            0 ) );
+        XI_ALLOC_AT( xi_ed_id_ed_context_pair_t, ed_id_context, state );
+
+        ed_id_context->edge_device_id = xi_str_dup( edge_device_id );
+
+        XI_CHECK_STATE(
+            state = xi_create_context_with_custom_layers_and_evtd(
+                &ed_id_context->edge_device_context, xi_gw_layer_chain, XI_LAYER_CHAIN_GW,
+                XI_LAYER_CHAIN_SCHEME_LENGTH( XI_LAYER_CHAIN_GW ), NULL, 0 ) );
+
+        const xi_vector_elem_t* e = xi_vector_push(
+            xi_globals.context_handles_vector_edge_devices,
+            XI_VEC_CONST_VALUE_PARAM( XI_VEC_VALUE_PTR( ed_id_context ) ) );
+
+        if ( NULL == e )
+        {
+            goto err_handling;
+        }
 
         /*
          * todo_atigyi:
          * - error handling
-         * - store application device id and xi_context_handle pair to map
          * - get corresponding credentials to application device id
          */
     }
@@ -77,7 +89,12 @@ xi_state_t xi_connect_ed( xi_context_handle_t xih, const char* edge_device_id )
         /* there is already such a context */
     }
 
+    return state;
+
 err_handling:
+
+    XI_SAFE_FREE( ed_id_context->edge_device_id );
+    XI_SAFE_FREE( ed_id_context );
 
     return state;
 }
@@ -86,16 +103,28 @@ xi_state_t xi_disconnect_ed( xi_context_handle_t xih, const char* edge_device_id
 {
     XI_UNUSED( xih );
 
-    xi_ed_id_ed_context_pair_t search_key = {.edge_device_id = edge_device_id,
-                                             XI_INVALID_CONTEXT_HANDLE};
+    xi_ed_id_ed_context_pair_t search_key = {.edge_device_id = ( char* )edge_device_id,
+                                             .edge_device_context = NULL};
 
     const xi_vector_index_type_t found_index = xi_vector_find(
         xi_globals.context_handles_vector_edge_devices,
         XI_VEC_CONST_VALUE_PARAM( ( void* )&search_key ), _xi_ed_find_context_2_id );
 
-    printf( "-- %s --, found_index: %d\n", __FUNCTION__, found_index );
+    if ( 0 <= found_index )
+    {
+        xi_ed_id_ed_context_pair_t* ed_id_context =
+            ( xi_ed_id_ed_context_pair_t* )xi_vector_get(
+                xi_globals.context_handles_vector_edge_devices, found_index );
 
-    XI_UNUSED( search_key );
+        xi_delete_context_with_custom_layers(
+            &ed_id_context->edge_device_context, xi_gw_layer_chain,
+            XI_LAYER_CHAIN_SCHEME_LENGTH( XI_LAYER_CHAIN_GW ) );
+
+        xi_vector_del( xi_globals.context_handles_vector_edge_devices, found_index );
+
+        XI_SAFE_FREE( ed_id_context->edge_device_id );
+        XI_SAFE_FREE( ed_id_context );
+    }
 
     return XI_STATE_OK;
 }
