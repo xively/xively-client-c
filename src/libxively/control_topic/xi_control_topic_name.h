edcb6643ba74208c615d6a5b02465af32539b27d
atigyi <tigyi.arpad@gmail.com>
2018-02-01 16:57:09 +0100

    [ GW ] control topic now builds up control topic name from device id passed in xi_connect and not from the one passed in xi_initialize. Reason: latter is stored in globals. Although edge devices should have control topic connections too, for instance to have OTA.

--- src/libxively/control_topic/xi_control_topic_name.h
+++ src/libxively/control_topic/xi_control_topic_name.h
@@ -22,7 +22,8 @@ extern const char* const XI_TOPIC_DOMAIN;
  * Creates and returns the platform specific topic name
  *
  */
-extern xi_state_t xi_control_topic_create_topic_name( char** subscribe_topic_name,
+extern xi_state_t xi_control_topic_create_topic_name( const char* device_id,
+                                                      char** subscribe_topic_name,
                                                       char** publish_topic_name );
 
 #ifdef __cplusplus
