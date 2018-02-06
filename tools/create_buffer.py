3d425e60050c4ee4ca6af38efe00c01bd3dc323e
Juan Carlos Jimenez <jcjc.dev@gmail.com>
2018-02-06 17:20:48 +0000

    memory_fs root cert list is now null-terminated It fixes certificate parsing for the mbedTLS BSP when using memory_fs. Other TLS BSPs can leave the null terminator out of the provided buffer; lowering the buffer len is easier and faster than reallocating the entire string just to append an extra char

--- tools/create_buffer.py
+++ tools/create_buffer.py
@@ -10,17 +10,27 @@ import os.path
 import struct
 from pprint import pprint
 
+h_file_name = ""
+c_file_name = ""
+
 PATTERN     = "0x%02x"
 NEWLINE     = "\n"
 INDENT      = 4 * " "
 PER_LINE    = 16
-
+ROOTCA_LIST_LEN_MACRO = "XI_ROOTCA_LIST_BYTE_LENGTH"
 def tabs_2_spaces( s ):
    return s.replace( "\t", "    " )
 
-h_pro       = tabs_2_spaces( "// Copyright (c) 2003-2018, LogMeIn, Inc. All rights reserved.\n// This is part of Xively C library." )
+h_pro = tabs_2_spaces( 
+"""\
+/* Copyright (c) 2003-2018, LogMeIn, Inc. All rights reserved.
+ *
+ * This is part of the Xively C Client library,
+ * it is licensed under the BSD 3-Clause license.
+ */
+""" )
 h_guard_beg = tabs_2_spaces( "#ifndef __%(name)s_H__\n#define __%(name)s_H__" )
-h_guard_end = tabs_2_spaces( "#endif // __%s_H__" )
+h_guard_end = tabs_2_spaces( "#endif /* __%s_H__ */" )
 cpp_beg     = tabs_2_spaces( "#ifdef __cplusplus\nextern \"C\" {\n#endif" )
 cpp_end     = tabs_2_spaces( "#ifdef __cplusplus\n}\n#endif" )
 
@@ -37,10 +47,15 @@ def load_file( file_name ):
             data.append( struct.unpack( 'B', byte )[ 0 ] )
             byte = f.read( 1 )
 
-    return data[ 0 : -1 ]
+    if data[-1] in [0x0a, 0x00]: #['\n', '\0']
+        data[-1] = 0x00
+    else:
+        data.append( 0x00 )
+
+    return data
 
 def convert_to_c_array( data, array_name ):
-    out     = "const unsigned char %s[ %d ] = {" % ( array_name, len( data ) )
+    out     = "const unsigned char %s[ %s ] = {" % ( array_name, ROOTCA_LIST_LEN_MACRO )
     out    += NEWLINE
     out    += INDENT
 
@@ -64,8 +79,11 @@ def convert_to_c_array( data, array_name ):
     return out
 
 def create_c_file( data, array_name ):
-    out  = h_pro + "\n\n"
+    out  = h_pro + "\n"
     out += cpp_beg + "\n\n"
+
+    out += '#include "%s"' % ( os.path.basename(h_file_name) )
+    out += "\n\n"
 
     out += convert_to_c_array( data, array_name )
     out += "\n\n"
@@ -77,7 +95,13 @@ def create_h_file( data, array_name ):
     out  = h_pro + "\n\n"
     out += ( h_guard_beg % { "name" : array_name.upper() } ) + "\n\n"
     out += cpp_beg + "\n\n"
-    out += "extern const unsigned char %s[ %d ];\n\n" % ( array_name, len( data ) )
+    out += "#ifndef %s" % ( ROOTCA_LIST_LEN_MACRO )
+    out += "\n"
+    out += "#define %s %d" % ( ROOTCA_LIST_LEN_MACRO, len( data ) )
+    out += "\n"
+    out += "#endif /* %s */" % ( ROOTCA_LIST_LEN_MACRO )
+    out += "\n\n"
+    out += "extern const unsigned char %s[ %s ];\n\n" % ( array_name, ROOTCA_LIST_LEN_MACRO )
     out += cpp_end + "\n\n"
     out += ( h_guard_end % ( array_name.upper() ) ) + "\n"
 
