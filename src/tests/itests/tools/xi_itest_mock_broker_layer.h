e1744a01581809a4df4527dc0cbe6a282e9c322f
atigyi <tigyi.arpad@gmail.com>
2018-02-07 14:19:58 +0100

    [ GW ] fix: PUBLISH message content was freed before it was used, so sometimes mock broker <—> xively client with edge device communication broke up with random sympthoms. Now memory management and lifetime handling is doen right.

--- src/tests/itests/tools/xi_itest_mock_broker_layer.h
+++ src/tests/itests/tools/xi_itest_mock_broker_layer.h
@@ -24,13 +24,9 @@ typedef struct xi_mock_broker_data_s
     const char* control_topic_name_broker_in;
     const char* control_topic_name_broker_out;
 
-    xi_data_desc_t* outgoing_publish_content;
-    xi_data_desc_t* outgoing_publish_content_secondary_layer;
-
     /* two mock broker contexts (mock broker and the mock broker edge device handler)
      * alternate the execution between each other. This solves the MQTT over MQTT codec
      * (double MQTT encoding / decoding) */
-
     xi_layer_t* layer_tunneled_message_target;
     xi_layer_t* layer_output_target;
 
