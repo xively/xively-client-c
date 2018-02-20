daf83f825b91fa671a9f861e19569d2d05e6cf5f
bszekely <balazs.szekely@autoindex.hu>
2018-02-21 00:07:57 +0100

    Adjusted current time with elapsed ticks

--- src/bsp/platform/stm32fx/xi_bsp_time_stm32fx.c
+++ src/bsp/platform/stm32fx/xi_bsp_time_stm32fx.c
@@ -13,17 +13,23 @@
 #endif
 #include <xi_bsp_time_stm32fx_sntp.h>
 
+static uint32_t ticks_at_start = 0;
+
 void xi_bsp_time_init()
 {
     xi_bsp_time_sntp_init( NULL );
+    if (xi_bsp_time_sntp_getseconds_posix() != 0)
+    {
+        ticks_at_start = HAL_GetTick();
+    }
 }
 
 xi_time_t xi_bsp_time_getcurrenttime_seconds()
 {
-    return xi_bsp_time_sntp_getseconds_posix() + HAL_GetTick() / 1000;
+    return xi_bsp_time_sntp_getseconds_posix() + (HAL_GetTick()-ticks_at_start) / 1000;
 }
 
 xi_time_t xi_bsp_time_getcurrenttime_milliseconds()
 {
-    return xi_bsp_time_sntp_getseconds_posix() + HAL_GetTick() / 1000;
+    return xi_bsp_time_sntp_getseconds_posix() + (HAL_GetTick()-ticks_at_start) / 1000;
 }
