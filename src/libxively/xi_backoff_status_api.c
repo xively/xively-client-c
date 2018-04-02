1b3284ad7f57b6e8ab2d61fb605d2ab62a9e58f9
Unknown <ddellabitta@gmail.com>
2018-04-02 16:14:02 -0400

    We should remove the decaly lut vector commpletely!

--- src/libxively/xi_backoff_status_api.c
+++ src/libxively/xi_backoff_status_api.c
@@ -95,8 +95,6 @@ void xi_cancel_backoff_event()
 void xi_reset_backoff_penalty()
 {
     xi_globals.backoff_status.backoff_lut_i = 0;
-    xi_globals.backoff_status.decay_lut_i   = 0;
-
     xi_cancel_backoff_event();
 }
 #endif
